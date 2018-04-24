8790bca93e79b0e6cb75a0e557ce640df9af1743
mvroij <mauro-vanroij@live.nl>
2018-04-24 15:40:46 +0200

    ui test aanmaak

---
+++ EcogramVR/Assets/togglebutton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class togglebutton : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(gameObject.name + " was triggered by " + other.gameObject.name);
+    }
+}
