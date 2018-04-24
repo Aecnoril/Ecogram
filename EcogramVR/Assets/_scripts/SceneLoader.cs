8cd1404fcfa2fcfe2832f0e691c06121a741aa73
Aecnoril <Aecnorilkoen@gmail.com>
2018-04-24 11:39:43 +0200

    SteamVR Teleport area fix
    Signed-off-by: Aecnoril <Aecnorilkoen@gmail.com>

---
+++ EcogramVR/Assets/_scripts/SceneLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Text loadingTip;
+    public Text loadingText;
+    public Texture2D progressBar;
+
+    [SerializeField]
+    private string sceneName = "Scenename";
+
+    private AsyncOperation async;
+
+    private void Start()
+    {
+        StartCoroutine(LoadLevel("LevelName"));
+        loadingText.text = "Loading...";
+    }
+
+    private IEnumerator LoadLevel(string Level)
+    {
+        async = SceneManager.LoadSceneAsync(sceneName);
+        yield return async;
+    }
+
+    private void RandomText()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        if (async != null)
+        {
+            GUI.DrawTexture(new Rect(0, 0, 100 * async.progress, 50), progressBar);
+        }
+    }
+
+}
