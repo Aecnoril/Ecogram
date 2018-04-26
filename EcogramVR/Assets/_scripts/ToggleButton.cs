6b02ec09d29437f98d68e6158a8f0ac5539e28b0
mvroij <mauro-vanroij@live.nl>
2018-04-26 15:15:47 +0200

    check for toggle gameobject  invoke toggle

---
+++ EcogramVR/Assets/_scripts/ToggleButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour
+{
+    private Toggle AssignedButton;
+
+    private void Awake()
+    {
+        AssignedButton = gameObject.GetComponent<Toggle>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log(gameObject.name + " was triggered by " + col.gameObject.name);
+        
+        AssignedButton.onValueChanged.Invoke(true);
+    }
+}
+
