82a9485da084489232e4bb0b4cb83ccb187ecd13
mvroij <mauro-vanroij@live.nl>
2018-05-02 14:32:23 +0200

    add tutorial scene

--- EcogramVR/Assets/_scripts/ToggleButton.cs
+++ EcogramVR/Assets/_scripts/ToggleButton.cs
@@ -5,18 +5,18 @@ using UnityEngine.UI;
 
 public class ToggleButton : MonoBehaviour
 {
-    
     private Toggle AssignedButton;
 
-    private void Awake()
+    private void Awake() //awake start direct als de game 
     {
         AssignedButton = gameObject.GetComponent<Toggle>();
     }
 
     private void OnTriggerEnter(Collider col)
     {
-        Debug.Log(gameObject.name + " was triggered by " + col.gameObject.name);
         Debug.Log(AssignedButton + "dit is de toggle button"); // check of assignedbutton wel vindbaar is. 
-        AssignedButton.isOn = !AssignedButton.isOn;   //assisnged button wordt wat assigned button niet is. dus uit is aan en aan is uit. 
+
+        AssignedButton.isOn = !AssignedButton.isOn;   //assisnged button wordt wat assigned button niet is. als hij niet aan is wordt hij aan)
     }
 }
+
---
+++ EcogramVR/Assets/_scripts/TriggerEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag ("Hand1"))
+        {
+            Debug.Log("WELKOM BOI");
+        }
+    }
+}
