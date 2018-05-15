51477f6238c119e352d49b92443a6e5b50adf10e
Aecnoril <Aecnorilkoen@gmail.com>
2018-05-15 13:58:51 +0200

    Added panel
    Signed-off-by: Aecnoril <Aecnorilkoen@gmail.com>

---
+++ EcogramVR/Assets/_scripts/ToggleButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour
+{
+    private Toggle AssignedButton;
+
+    private void Awake() //awake start direct als de game 
+    {
+        AssignedButton = gameObject.GetComponent<Toggle>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(AssignedButton + "dit is de toggle button"); // check of assignedbutton wel vindbaar is. 
+
+        AssignedButton.isOn = !AssignedButton.isOn;   //assisnged button wordt wat assigned button niet is. als hij niet aan is wordt hij aan)
+    }
+}
+
