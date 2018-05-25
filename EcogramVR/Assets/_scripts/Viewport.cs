c047f17a75e692a71deeac724ca341492e006a05
Aecnoril <Aecnorilkoen@gmail.com>
2018-05-25 12:43:27 +0200

    Viewport on menu
    Signed-off-by: Aecnoril <Aecnorilkoen@gmail.com>

---
+++ EcogramVR/Assets/_scripts/Viewport.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viewport : MonoBehaviour {
+
+    [SerializeField]
+    private Collider viewportHitbox;
+    [SerializeField]
+    private GameObject enabledObjects;
+
+    private Transform cameraTransform;
+
+    private bool isHit = false;
+
+	// Use this for initialization
+	void Start () {
+        cameraTransform = Camera.main.transform;
+
+        if (viewportHitbox == null)
+            viewportHitbox = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, 50))
+            isHit = true;
+        else
+            isHit = false;
+
+            enabledObjects.SetActive(isHit);
+	}
+}
