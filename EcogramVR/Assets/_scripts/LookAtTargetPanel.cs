d5840bd0953ed15ea7701a074f507229813e9804
Aecnoril <Aecnorilkoen@gmail.com>
2018-06-04 18:10:24 +0200

    Yes?
    Signed-off-by: Aecnoril <Aecnorilkoen@gmail.com>

---
+++ EcogramVR/Assets/_scripts/LookAtTargetPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTargetPanel : MonoBehaviour {
+
+    public Transform target;
+
+    private void Start()
+    {
+        target = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+
+            Vector3 lookPos = target.position;
+            lookPos.y = transform.position.y;
+            transform.LookAt(lookPos);
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
+
+    }
+}
