926056b16477c4f087bbe9d87daab433aa4ad3b6
Aecnoril <Aecnorilkoen@gmail.com>
2018-06-08 15:30:20 +0200

    Laser pointer
    Signed-off-by: Aecnoril <Aecnorilkoen@gmail.com>

---
+++ EcogramVR/Assets/_scripts/Input/LaserPointer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour
+{
+
+    private Transform laserOriginObject;
+    private LineRenderer lineRender;
+
+    public GameObject pointedObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        laserOriginObject = gameObject.transform;
+        lineRender = gameObject.AddComponent<LineRenderer>();
+        lineRender.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(laserOriginObject.position, laserOriginObject.forward, out hit, 25f, LayerMask.GetMask("ControllerCollider")))
+        {
+            lineRender.SetPositions(new Vector3[2] { laserOriginObject.position, hit.point });
+
+            lineRender.useWorldSpace = true;
+            lineRender.enabled = true;
+
+        };
+
+
+
+    }
+}
